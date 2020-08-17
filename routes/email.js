7e2e0319b50bf87370ca3dfdf2f72e1d412fa2af
PAHJunior <pahjunior@outlook.com>
2020-08-16 23:45:07 -0300

    first commit

---
+++ routes/email.js
@@ -0,0 +1,39 @@
+"use strict"
+
+const express = require('express');
+const router = express.Router();
+const nodemailer = require("nodemailer");
+
+router.post('/', async (req, res, next) => {
+  const userMail = '...@outlook.com'
+  const userName = ''
+  const passMail = ''
+
+  let transporter = nodemailer.createTransport({
+    host: "SMTP.office365.com",
+    port: 587,
+    tls: {
+      ciphers: 'SSLv3'
+    },
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: userMail, // generated ethereal user
+      pass: passMail // generated ethereal password
+    }
+  })
+
+  let html = `Corpo do e-mail`
+
+  await transporter.sendMail({
+    from: `"${userName}" <${userMail}>`, // sender address
+    to: "...@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    html: html // html body
+  })
+
+  res.send({
+    msg: 'Email enviado com sucesso'
+  });
+});
+
+module.exports = router;
